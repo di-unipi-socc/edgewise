feaaaf2592f0fb72af49e9fa5850d4ca9ea3d1f2
Jacopo Massa <jacopomassa97@gmail.com>
2022-11-07 10:11:21 +0000

    upload experiment img results

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -8,6 +8,7 @@ from googleOR.classes.utils import DATA_DIR, EXPERIMENTS_DIR
 
 sizes = ["16", "32", "64", "128", "256", "512"]
 x = [i for i in range(len(sizes))]
+
 def time_vs(df):
     sns.set(style="whitegrid")
     sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
@@ -27,12 +28,11 @@ def change_vs(df):
     sns.set(style="whitegrid")
     sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
     plt.figure(figsize=(10, 6))
-
-    sns.pointplot(x=x, y="Change", data=df, hue="Version", palette="Set2", errorbar=False)
+    sns.lineplot(x="Size", y="Change", data=df, hue="Version", palette="Set2")
     plt.xlabel("Infrastructure Size")
     plt.ylabel("Change")
     plt.title("Change vs Size")
-    plt.legend(loc='upper left')
+    plt.legend(loc='lower right')
     plt.savefig(os.path.join(EXPERIMENTS_DIR, "change_vs_size.png"), dpi=600)
     plt.close()
     print("Change vs Size, DONE")
@@ -55,9 +55,6 @@ def main(df):
     time_vs(df)
     change_vs(df)
     bins_vs(df)
-
-    # bin_cost_plus(df)
-    # tradeoff_plus(df)
 
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join(DATA_DIR, 'results.csv'))
