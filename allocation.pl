17a01856d350ab5e547a3901d020579b4f5c6067
Jacopo <jacopomassa97@gmail.com>
2023-01-23 18:11:55 +0100

    Update allocation.pl

--- allocation.pl
+++ allocation.pl
@@ -1,29 +1,22 @@
-resourceAllocation(Key, ReqRes, TAlloc, [(Key, NewRes)|Rest]) :-
-    select((Key, Res), TAlloc, Rest), NewRes is Res+ReqRes.
-resourceAllocation(Key, ReqRes, TAlloc, [(Key, ReqRes)|TAlloc]).
-
-bwAllocation(N1, N2, ReqBW, TBW, [(N1, N2, NewBW)|BWs]) :-
-    select((N1, N2, BW), TBW, BWs), NewBW is BW+ReqBW.
-bwAllocation(N1, N2, ReqBW, TBW, [(N1, N2, ReqBW)|TBW]).
-
-/*
-hwAllocation(N, ReqHW, THW, [(N, NewHW)|Hws]) :-
-    select((N,HW), THW, HWs), NewHW is HW+ReqHW.
-hwAllocation(N, ReqHW, THW, [(N, ReqHW)|THW]).
-*/
+:-['data/infrs/infr128.pl', 'data/apps/speakToMe.pl'].
 
 hwOnN(N, Ps, HW) :- serviceInstance(S, SId), service(SId,_,(_,HW)), member((S,N), Ps).
 hwOnN(N, Ps, HW) :- functionInstance(F, FId,_), function(FId,_,(_,HW)), member((F,N), Ps).
 
-hwAllocation([N|Ns], P, ) :-
-    findall(HW, hwOnN(N, P, HW), HWs), sum_list(HWs,TotHW),
-    hwAllocation([Ns], P, [()])
+hwAllocation([], _, AllocHW, AllocHW).
+hwAllocation([N|Ns], P, OldHW, AllocHW) :-
+    findall(HW, hwOnN(N, P, HW), HWs), sum_list(HWs, TotHW),
+    hwAllocation(Ns, P, [(N, TotHW)|OldHW], AllocHW).
 
-/*
-hwAllocation(P,AllocHW) :-
-    findall((N,HW), (member(on(T,N), P), (service(T,_,HW,_,_)), HWs),
-    findall((N,SumHW), (group_by(N, HW, member((N,HW),HWs), Group), sumHW(Group, SumHW)), AllocHW).
-*/
+bwOnN((N1,N2), Ps, BW) :- 
+    dataFlow(T1, T2, _, _, Size, Rate, _),
+    (member((T1,N1), Ps); node(N1, _, _, _, _, IoTCaps), member(T1, IoTCaps)),
+    (member((T2,N2), Ps); node(N2, _, _, _, _, IoTCaps), member(T2, IoTCaps)),
+    BW is Size*Rate.
 
-bwAllocation(P,AllocBW) :-
-    findall()
+resourceAllocation(Ps, AllocHW, AllocBW) :-
+    findall(N, distinct(member((_, N), Ps)), Ns),
+    hwAllocation(Ns, Ps, [], AllocHW), AllocBW = [].
+
+start(HW,BW) :- % testing predicate
+    resourceAllocation([(textBucket,n105),(audioBucket,n105),(mainDB,n105),(convertTxt,n123),(uploadPost,n105),(uploadAudio,n105),(publishPost,n33),(converter,n123),(postQueue,n105),(metaPost,n123),(metaAudio,n123)], HW,BW).
