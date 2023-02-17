b1ee3a2e9c1c7ad8374ee32cfc0ed7265a2346f2
jacopo-massa <jacopomassa97@gmail.com>
2023-02-17 10:29:59 +0000

    update git ignore

--- scripts/plot.py
+++ scripts/plot.py
@@ -11,15 +11,26 @@ sizes = ["16", "32", "64", "128", "256", "512"]
 x = [i for i in range(len(sizes))]
 
 
-def size_vs(field, df):
+def size_vs(field, df, legend=True, lineplot=True, logy=False):
+    # set seaborn context
     sns.set(style="whitegrid")
     sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+
+    # choose plot
     plt.figure(figsize=(10, 6))
-    sns.barplot(x="Size", y=field, data=df, hue="Version", palette="Set2")
+    if lineplot:
+        sns.lineplot(x="Size", y=field, data=df, hue="Version", palette="Set2")
+    else:
+        sns.barplot(x="Size", y=field, data=df, hue="Version", palette="Set2")
+    
+    # set labels and y-scale
     plt.xlabel("Infrastructure Size")
     plt.ylabel(field)
+    plt.yscale('log') if logy else None
     plt.title("{} vs Size".format(field))
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper left') if legend else plt.legend([],[], frameon=False)
+
+    # save plot
     plt.savefig(join(PLOTS_DIR, "{}_vs_size.png".format(field.lower())), dpi=600)
     plt.close()
     print("{} vs Size, DONE".format(field))
@@ -33,8 +44,8 @@ if __name__ == '__main__':
 
     try:
         df = pd.read_csv(COMPARISON_FILE)
-        size_vs("Time", df)
+        size_vs("Time", df, logy= True)
         size_vs("Change", df)
-        size_vs("Bins", df)
+        size_vs("Bins", df, lineplot=False)
     except FileNotFoundError as e:
         print(Fore.LIGHTRED_EX + "File not found: {}.".format(basename(e.filename)))
