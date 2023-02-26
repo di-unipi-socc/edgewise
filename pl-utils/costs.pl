b71fbe9ccbe88cf6a824a1f7a24f7d40501c6b3f
Jacopo Massa <jacopomassa97@gmail.com>
2023-02-26 22:23:06 +0100

    Update costs.pl

--- pl-utils/costs.pl
+++ pl-utils/costs.pl
@@ -51,38 +51,26 @@ weight(F, Rank) :- functionInstance(F, FId, _), function(FId, _, (_, Rank)).
 % unitCost(Cap, NType, Cost).
 % SOFTWARE
 unitCost(ubuntu, cloud, 5.524).
-unitCost(ubuntu, isp, 2.826).
-unitCost(ubuntu, cabinet, 0.987).
-unitCost(ubuntu, accesspoint, 0.505).
+unitCost(ubuntu, edge, 1.987).
 unitCost(ubuntu, thing, 0.26).
 
 unitCost(python, cloud, 4.608).
-unitCost(python, isp, 2.304).
-unitCost(python, cabinet, 0.768).
-unitCost(python, accesspoint, 0.384).
+unitCost(python, edge, 1.768).
 unitCost(python, thing, 0.192).
 
 unitCost(gcc, cloud, 19.347).
-unitCost(gcc, isp, 10.656).
-unitCost(gcc, cabinet, 5.558).
-unitCost(gcc, accesspoint, 2.686).
+unitCost(gcc, edge, 15.558).
 unitCost(gcc, thing, 1.544).
 
 unitCost(mySQL, cloud, 3.2).
-unitCost(mySQL, isp, 2.25).
-unitCost(mySQL, cabinet, 0.8).
-unitCost(mySQL, accesspoint, 0.4).
+unitCost(mySQL, edge, 1.8).
 unitCost(mySQL, thing, 0.2).
 
 % HARDWARE
 unitCost(arm64, cloud, 3.226).
-unitCost(arm64, isp, 1.848).
-unitCost(arm64, cabinet, 0.616).
-unitCost(arm64, accesspoint, 0.308).
+unitCost(arm64, edge, 1.616).
 unitCost(arm64, thing, 0.154).
 
 unitCost(x86, cloud, 7.104).
-unitCost(x86, isp, 3.552).
-unitCost(x86, cabinet, 0.928).
-unitCost(x86, accesspoint, 0.464).
+unitCost(x86, edge, 2.928).
 unitCost(x86, thing, 0.232).
