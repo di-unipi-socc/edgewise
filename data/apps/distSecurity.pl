38a840952519c704cd96beb0b80cc172fc3159ce
Jacopo Massa <jacopomassa97@gmail.com>
2023-02-26 22:07:06 +0100

    try some sec reqs

--- data/apps/distSecurity.pl
+++ data/apps/distSecurity.pl
@@ -30,13 +30,13 @@ thingInstance(cam21, secCam).
 thingInstance(cam22, secCam).
 
 % dataFlow(Source, Dest, DataId, SecReqs, Size, Rate, MaxLat).
-dataFlow(cam11, mainDB, detection, [enc], 0.02, 80, 20).
-dataFlow(cam21, mainDB, detection, [enc], 0.02, 80, 20).
-dataFlow(cam12, detector, detection, [enc], 0.02, 80, 20).
-dataFlow(cam22, detector, detection, [enc], 0.02, 80, 20).
-dataFlow(detector, recognizer, bbox, [enc], 0.03, 50, 20).
-dataFlow(recognizer, traje, plateNumber, [enc], 0.01, 50, 20).
-dataFlow(recognizer, mainDB, detection, [enc], 0.02, 50, 50).
-dataFlow(mainDB, traje, detection, [enc], 0.1, 50, 20).
-dataFlow(traje, logger, detection, [enc], 0.02, 50, 20).
-dataFlow(logger, mainDB, status, [enc], 0.05, 50, 20).
+dataFlow(cam11, mainDB, detection, [enc_iot, enc_storage], 0.02, 80, 20).
+dataFlow(cam21, mainDB, detection, [enc_iot, enc_storage], 0.02, 80, 20).
+dataFlow(cam12, detector, detection, [enc_iot, wireless_security], 0.02, 80, 20).
+dataFlow(cam22, detector, detection, [enc_iot, wireless_security], 0.02, 80, 20).
+dataFlow(detector, recognizer, bbox, [anti_tampering], 0.03, 50, 20).
+dataFlow(recognizer, traje, plateNumber, [anti_tampering], 0.01, 50, 20).
+dataFlow(recognizer, mainDB, detection, [anti_tampering, enc_storage], 0.02, 50, 50).
+dataFlow(mainDB, traje, detection, [anti_tampering, node_isolation], 0.1, 50, 20).
+dataFlow(traje, logger, detection, [user_data_isolation], 0.02, 50, 20).
+dataFlow(logger, mainDB, status, [enc_storage, public_key_crypto], 0.05, 50, 20).
--- scripts/classes/builder.py
+++ scripts/classes/builder.py
@@ -22,19 +22,19 @@ THINGS 			= ['soil', 'heat', 'water', 'nutrient', 'energy', 'piCamera1', 'piCame
 SEC_CAPS_CLOUD 	= ['access_logs', 'authentication', 'host_ids', 'process_isolation', 'permission_model', 'resource_monitoring', 'restore_point', 'user_data_isolation', # virtualisation
                 'certificates', 'firewall', 'enc_iot', 'node_isolation', 'network_ids', 'public_key_crypto', # communications
                 'backup', 'enc_storage', # data
-                'access_control', 'anti-tampering', # physical
+                'access_control', 'anti_tampering', # physical
                 'audit'] # others
 
 SEC_CAPS_EDGE 	= ['access_logs', 'authentication', 'host_ids', 'process_isolation', 'permission_model', 'resource_monitoring', 'user_data_isolation', # virtualisation
                 'certificates', 'firewall', 'enc_iot', 'node_isolation', 'network_ids', 'public_key_crypto', # communications
-                'backup', 'enc_storage', # data,
-                'access_control', 'anti-tampering', # physical
+                'backup', 'enc_storage', 'obfuscated_storage', # data,
+                'access_control', 'anti_tampering', # physical
                 'audit'] # others
 
 SEC_CAPS_IOT 	= ['authentication', 'resource_monitoring', # virtualisation
                 'firewall', 'enc_iot', 'node_isolation', 'public_key_crypto', 'wireless_security' # communications
                 'enc_storage', 'obfuscated_storage', # data,
-                'anti-tampering'] # physical
+                'anti_tampering'] # physical
                 # others
 
 TYPES_PROBS 	= [0.15, 0.45, 0.4]
