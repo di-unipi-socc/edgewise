fb7a12db34f02095379c8854578b7121a9f783af
jacopo-massa <jacopomassa97@gmail.com>
2023-03-01 18:29:45 +0000

    add comment

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -6,6 +6,7 @@ INFRS_DIR=$ROOT_DIR/data/infrs
 PYTHON=$ROOT_DIR/.venv/bin/python3
 PYTHON_SCRIPTS_DIR=$ROOT_DIR/scripts
 
+# remove all old infrastructure files
 rm $INFRS_DIR/*.pl > /dev/null 2>&1
 for i in {0..5}; do
     ${PYTHON} ${PYTHON_SCRIPTS_DIR}/classes/builder.py ${INFRS[$i]} # -s ${INFRS[$i]}
