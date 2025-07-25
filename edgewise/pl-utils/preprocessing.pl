257ab710837deb8abc322b14decd4bfce90f20ac
Jacopo Massa <jacopomassa97@gmail.com>
2025-07-25 12:18:35 +0200

    fix: Add CurrP in preprocessing

--- edgewise/pl-utils/preprocessing.pl
+++ edgewise/pl-utils/preprocessing.pl
@@ -1,6 +1,8 @@
 :-['requirements.pl', 'costs.pl'].
 :- dynamic deployed/2.
 
+% :- ['../applications/prolog/speakToMe.pl', '../infrastructures/BA/infr64-151195.pl'].
+
 
 :- set_prolog_flag(answer_write_options,[max_depth(0)]). % write answers' text entirely
 :- set_prolog_flag(stack_limit, 128 000 000 000).
@@ -15,45 +17,46 @@ preprocess(App, Compatibles) :-
     deployed(App, Placement),
     crStep(Placement, Compatibles).
 
-crStep(P, Compatibles) :- crStep(P, [], Compatibles).
-crStep([(C,N)|P], POk, [(C,[(N,Cost)])|Cs]) :-
-    componentPlacement(C, N, P), qosOK(C, N, POk),
+crStep(P, Compatibles) :- crStep(P, P, [], Compatibles).
+crStep([(C,N)|P], CurrP, POk, [(C,[(N,Cost)])|Cs]) :-
+    componentPlacement(C, N, CurrP, POk), qosOK(C, N, CurrP, POk),
     nodeType(N, Type), cost(Type, C, Cost),
-    crStep(P, [(C,N)|POk], Cs).
-crStep([(C,N)|P], POk, [(C,[Comp|Atibles])|Cs]) :-
-    \+ ( componentPlacement(C, N, P), qosOK(C, N, POk) ), 
+    crStep(P, CurrP, [(C,N)|POk], Cs).
+crStep([(C,N)|P], CurrP, POk, [(C,[Comp|Atibles])|Cs]) :-
+    \+ ( componentPlacement(C, N, CurrP, POk), qosOK(C, N, CurrP, POk) ),
     findall((M, Cost), lightNodeOK(C, M, Cost), [Comp|Atibles]),
-    crStep(P, POk, Cs).
-crStep([], _, []).
+    crStep(P, CurrP, POk, Cs).
+crStep([], _, _, []).
 
-componentPlacement(F, N, P) :-
+componentPlacement(F, N, CurrP, P) :-
     functionInstance(F, FId, _), function(FId, SWPlat, (Arch,HWReqs)),
     node(N, SWCaps, (Arch,HWCaps), _, _), 
     requirements(FId, N), 
     member(SWPlat, SWCaps),
-    hwOK(N, HWCaps, HWReqs, P).
-componentPlacement(S, N, P) :-
+    hwOK(N, HWCaps, HWReqs, CurrP, P).
+componentPlacement(S, N, CurrP, P) :-
     serviceInstance(S, SId), service(SId, SWReqs, (Arch,HWReqs)),
     node(N, SWCaps, (Arch,HWCaps), _, _), 
     requirements(SId, N), 
     subset(SWReqs, SWCaps),
-    hwOK(N, HWCaps, HWReqs, P).
+    hwOK(N, HWCaps, HWReqs, CurrP, P).
 
 checkThings :-
     findall(T, thingInstance(T, _), Things),
     findall(T, (node(_, _, _, _, IoTCaps), member(T, IoTCaps)), IoT),
     subset(Things, IoT).
 
-qosOK(C, N, P) :- 
+qosOK(C, N, CurrP, P) :- 
     findall(DF, relevant(C, N, P, DF), DataFlows), 
-    checkDF(DataFlows, [(C,N)|P]).
+    checkDF(DataFlows, CurrP, [(C,N)|P]).
 
 hwOnN(N, Ps, HW) :- serviceInstance(S, SId), service(SId,_,(_,HW)), member((S,N), Ps).
 hwOnN(N, Ps, HW) :- functionInstance(F, FId,_), function(FId,_,(_,HW)), member((F,N), Ps).
 
-hwOK(N, HWCaps, HWReqs, P) :- 
+hwOK(N, HWCaps, HWReqs, CurrP, P) :- 
     findall(H, hwOnN(N, P, H), Hs), sum_list(Hs, UsedHW),
-    hwTh(T), HWCaps >= HWReqs + UsedHW + T.
+    findall(H, hwOnN(N, CurrP, H), CurrHs), sum_list(CurrHs, CurrHw),
+    hwTh(T), HWCaps + CurrHw >= HWReqs + UsedHW + T.
 
 relevant(C, N, P, (N, M, Lat, BW, Sec)):-
     dataFlow(C, C1, _, Sec, Size, Rate, Lat),
@@ -64,22 +67,22 @@ relevant(C, N, _, (M, N, Lat, BW, Sec)):-
     node(M, _, _, _, IoTCaps), member(C1, IoTCaps),
     BW is Size*Rate.
 
-checkDF([(N1,N2, ReqLat, _, SecReqs)|DFs], [(C,N)|P]) :-
+checkDF([(N1,N2, ReqLat, _, SecReqs)|DFs], CurrP, [(C,N)|P]) :-
     secOK(N1, N2, SecReqs),
     link(N1, N2, FeatLat, FeatBW),
     FeatLat =< ReqLat,
-    bwOK(C, N, FeatBW, P),
-    checkDF(DFs, [(C,N)|P]).
-checkDF([], _).
+    bwOK(C, N, FeatBW, CurrP, P),
+    checkDF(DFs, CurrP, [(C,N)|P]).
+checkDF([], _, _).
 
 secOK(N1, N2, SecReqs) :-
     node(N1, _, _, SecCaps1, _), subset(SecReqs, SecCaps1),
     node(N2, _, _, SecCaps2, _), subset(SecReqs, SecCaps2).
 
-bwOK(C, N, FeatBW, P):-
+bwOK(C, N, FeatBW, CurrP, P):-
     findall(BW, relevant(C, N, P, (_,_,_,BW,_)), BWs), sum_list(BWs, OkAllocBW), 
-    bwTh(T), FeatBW >= OkAllocBW + T.
-
+    findall(BW, relevant(C, N, CurrP, (_,_,_,BW,_)), CurrBWs), sum_list(CurrBWs, CurrAllocBW),
+    bwTh(T), FeatBW + CurrAllocBW >= OkAllocBW + T.
 
 findCompatibles([C|Cs], [(C,[Comp|Atibles])|Rest]):-
     findCompatibles(Cs, Rest),
